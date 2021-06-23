1bbbdd0dfe2552f7c04248654beab31379affe5e
AlejandroSotoCastro <alejandrosc.eng@gmail.com>
2021-06-23 15:50:57 +0200

    some cleaning

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const loggerMiddleWare = require("morgan");
+
 const cors = require("cors");
 
 const dbConfig = require("./config/constants.js");
+const { PORT } = require("./config/constants");
 
 const authRouter = require("./routers/auth");
 const sideRouter = require("./routers/sidekicks");
-
-const PORT = process.env.PORT || 4000;
 
 // Connect to MongoDB database
 mongoose
@@ -16,6 +17,8 @@ mongoose
     console.log("conected to DB");
     const app = express();
 
+    app.use(loggerMiddleWare("dev"));
+
     app.use(cors());
 
     // parse requests of content-type - application/json
@@ -23,6 +26,12 @@ mongoose
 
     // parse requests of content-type - application/x-www-form-urlencoded
     app.use(express.urlencoded({ extended: true }));
+
+    if (process.env.DELAY) {
+      app.use((req, res, next) => {
+        setTimeout(() => next(), parseInt(process.env.DELAY));
+      });
+    }
 
     // simple route
     app.get("/", (req, res) => {
@@ -38,49 +47,3 @@ mongoose
       console.log(`Server is running on port ${PORT}.`);
     });
   });
-
-/*
-const db = require("./models");
-db.mongoose
-  .connect(db.url, {
-    // dbName: "sidekicks",
-    // user: "admin",
-    // pass: "admin",
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    useCreateIndex: true,
-  })
-  .then(async () => {
-    console.log("Connected to the database!");
-    const User = db.user;
-    // // Create a User
-    // const user = new User({
-    //   title: "title",
-    //   description: "description",
-    //   published: false,
-    // });
-    // const savedUser = await user
-    //   .save(user)
-
-    //   .catch((err) => {
-    //     console.log(err.message, "Some error occurred");
-    //   });
-    // console.log(savedUser);
-    User.find()
-      .then((data) => {
-        console.log(data);
-      })
-      .catch((err) => {
-        console.log({
-          message:
-            err.message || "Some error occurred while retrieving tutorials.",
-        });
-      });
-  })
-  .catch((err) => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
-
-  */
